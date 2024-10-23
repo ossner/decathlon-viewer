283cc666868e123501c5001559e0b32005d7a11a
Sebastian Oßner <github@sebastianoner.com>
2024-10-23 20:06:39 +0200

    Add README and some error checking

---
+++ README.md
@@ -0,0 +1,38 @@
+# Interactive Viewer for the Medical Segmentation Decathlon
+## [Dataset Link](https://drive.google.com/drive/folders/1HqEgzS8BV2c7xYNrZdEAnrHk7osJJ--2)
+## [Paper Link](https://www.nature.com/articles/s41467-022-30695-9)
+## Setup
+Download dataset so that:
+```
+data/TaskXX_Organ/images...
+```
+Example:
+
+![img.png](img.png)
+
+Create a new conda environment
+```
+conda create -n decathlon python=3.10
+```
+
+Activate Environment
+```
+conda activate decathlon
+```
+
+Install dependencies
+```
+pip install -r requirements.txt
+```
+## Running
+Run App
+```
+marimo run notebook.py
+```
+
+Edit Notebook
+```
+marimo edit notebook.py
+```
+
+## [Marimo Documentation](https://docs.marimo.io/)
--- notebook.py
+++ notebook.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.8.0"
+__generated_with = "0.9.0"
 app = marimo.App(width="medium", app_title="Dataviewer")
 
 
@@ -18,7 +18,7 @@ def __():
 
 
 @app.cell
-def __(BytesIO, Image, Normalize, nib, np, plt):
+def __(BytesIO, Image, Normalize, nib, np, plt, sliderAlph):
     def load_nii(file_path):
         img = nib.load(file_path)
         return img.get_fdata()
@@ -27,25 +27,25 @@ def __(BytesIO, Image, Normalize, nib, np, plt):
         # Normalize the image for better visualization
         norm = Normalize(vmin=np.min(image), vmax=np.max(image))
         image = norm(image)
-        
+
         overlays = []
-        
+
         # Iterate through slices and create overlays
         for slice_idx in range(image.shape[2]):
             fig, ax = plt.subplots()
             ax.imshow(image[:, :, slice_idx], cmap="gray")
-            ax.imshow(label[:, :, slice_idx], cmap="jet", alpha=0.2)
+            ax.imshow(label[:, :, slice_idx], cmap="jet", alpha=sliderAlph.value)
             ax.axis('off')
-            
+
             # Save the plot to memory (BytesIO) instead of displaying it
             buf = BytesIO()
             plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
             buf.seek(0)
-            
+
             # Convert the buffer to an image (PIL Image)
             overlay_image = Image.open(buf)
             overlays.append(overlay_image)
-            
+
             plt.close(fig)  # Close the plot to free memory
 
         return overlays
@@ -66,36 +66,46 @@ def __(dropdownDir, dropdownImg, load_image, load_nii):
 
 @app.cell
 def __(mo, os):
+    _organDirs = list(filter(lambda x: os.path.isdir(os.path.join('data/', x)), sorted(os.listdir('data/'))))
+    mo.stop(len(_organDirs) == 0, mo.md('**ERROR:** *data directory does not have subdirectories*'))
     dropdownDir = mo.ui.dropdown(
-        options=sorted(os.listdir('data/')), value="Task07_Pancreas", label="Choose Organ"
+        options=_organDirs, value=_organDirs[0], label="Choose Organ"
     )
-    return dropdownDir,
+    return (dropdownDir,)
 
 
 @app.cell
 def __(dropdownDir, mo, os):
+    _organIms = list(filter(lambda x: x.endswith('.nii.gz'), os.listdir(f'data/{dropdownDir.value}/imagesTr/')))
     dropdownImg = mo.ui.dropdown(
-        options=sorted(os.listdir(f'data/{dropdownDir.value}/imagesTr/')), value=sorted(os.listdir(f'data/{dropdownDir.value}/imagesTr/'))[0], label="Choose Image"
+        options=_organIms, value=_organIms[0], label="Choose Image"
     )
-    return dropdownImg,
+    return (dropdownImg,)
 
 
 @app.cell
 def __(mo, overlays):
-    slider = mo.ui.slider(start=0, stop=len(overlays) - 1, step=1)
-    return slider,
+    sliderSlc = mo.ui.slider(start=0, stop=len(overlays) - 1, step=1, label="Slice")
+    return (sliderSlc,)
 
 
 @app.cell
-def __(dropdownDir, dropdownImg, mo, slider):
-    mo.vstack([dropdownDir, dropdownImg, slider])
+def __(mo):
+    sliderAlph = mo.ui.slider(start=0, stop=1, step=0.05, value=0.2, label="Overlay alpha (requires reload)")
+    return (sliderAlph,)
+
+
+@app.cell
+def __(dropdownDir, dropdownImg, mo, sliderAlph, sliderSlc):
+
+    mo.vstack([dropdownDir, dropdownImg, sliderSlc, sliderAlph])
     return
 
 
 @app.cell
-def __(overlays, plt, slider):
+def __(overlays, plt, sliderSlc):
     plt.figure(figsize=(8, 8))
-    plt.imshow(overlays[slider.value])
+    plt.imshow(overlays[sliderSlc.value])
     plt.axis('off')
     plt.gca()
     return
